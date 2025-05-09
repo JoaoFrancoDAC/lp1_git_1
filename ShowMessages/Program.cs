f27d0744ba2a7d110f19dae46efc0aef287b1eba
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-05-09 16:37:12 +0100

    Showanother message

--- ShowMessages/Program.cs
+++ ShowMessages/Program.cs
@@ -6,7 +6,7 @@ namespace ShowMessages
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("My commit");
         }
     }
 }
